d4e9e03a3963dbfbd5941b9c1a6c9ab0ea7e5f70
lizheng <lizheng1025@airdoc.com>
2023-11-13 10:43:55 +0800

    feat |> 支持kmodule

--- root_zwsample/driver/module/xxx_config.h
+++
@@ -1,7 +0,0 @@
-#ifndef __XXX_CONFIG_H__
-#define __XXX_CONFIG_H__
-__BEGIN_DECLS
-#include "xxx_type.h"
-
-__END_DECLS
-#endif
--- root_zwsample/driver/module/xxx_type.h
+++
@@ -1,7 +0,0 @@
-#ifndef __XXX_TYPE_H__
-#define __XXX_TYPE_H__
-__BEGIN_DECLS
-
-__END_DECLS
-#endif
-
